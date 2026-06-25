2947b951674d1f221c0a1f046450f1c549bc18b2
Metrine0987 <metrine.regina@gmail.com>
2026-06-25 16:32:17 +0300

    fix: correct admin setup, custom product admin, and tag models

--- store/admin.py
+++ store/admin.py
@@ -1,13 +1,36 @@
-from django.contrib import admin
+from django.contrib import admin , messages
 from . import models
 from django.db.models import Count
 
+
+
+class InventoryFilter(admin.SimpleListFilter):
+    title = 'inventory'
+    parameter_name ='inventory'
+    
+    def lookups(self, request, model_admin):
+        return [
+            ('<10' , 'Low')
+        ]
+    
+    
+    def queryset(self, request, queryset):
+        if self.value() == '<10':
+           return queryset.filter(inventory__lt=10)
+    
+        
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['collection']
+    search_fields=['title']
+    prepopulated_fields = {
+        'slug' : ['title']}
+    actions = ['clear_inventory']
     list_display = ['title' , 'unit_price' , 'inventory_status' , 'collection_title']
     list_editable = ['unit_price' ]
     list_per_page = 10
     list_select_related = ['collection']
+    list_filter = ['collection', 'last_update' , InventoryFilter]
 
     def collection_title(self , product):
         return product.collection.title
@@ -17,10 +40,20 @@ class ProductAdmin(admin.ModelAdmin):
         if product.inventory < 10:
             return 'Low'
         return 'OK'
+    
+    @admin.action(description = 'Clear Inventory')    
+    def clear_inventory(self, request, queryset):
+        updated_count = queryset.update(inventory=0)
+        self.message_user(
+            request,
+            f'{updated_count} products were successfully updated.',
+            messages.ERROR
+        )
 
 
 @admin.register(models.Collection)
 class CollectionAdmin(admin.ModelAdmin):
+    search_fields = ['title']
     list_display = ['title', 'products_count']
     
     @admin.display(ordering='products_count')
@@ -34,12 +67,26 @@ class CollectionAdmin(admin.ModelAdmin):
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ['first_name' , 'last_name' , 'membership']
+    list_display = ['first_name' , 'last_name' , 'membership' ]
     list_editable = ['membership']
-    ordering = ['first_name' , 'last_name']
     list_per_page = 10
+    ordering = ['first_name' , 'last_name']
+    search_fields = ['first_name__istartswith', 'last_name__istartswith']
+    
+class OrderItemInline(admin.StackedInline):
+    model = models.OrderItem
+    autocomplete_fields = ['product']
+    min_num=0
+    max_num=10
+    extra = 0
+    
+    
     
 @admin.register(models.Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['id' ,'placed_at' ,'customer']
+    autocomplete_fields = ['customer']
+    inlines = [OrderItemInline]
+    list_display = ['id' ,'placed_at' ,'customer' , 'payment_status']
+    list_editable = ['payment_status']
+    
     
--- tags/models.py
+++ tags/models.py
@@ -1,3 +1,29 @@
-from django.db import models
+from django.db import models 
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
 
 # Create your models here.
+class Tag(models.Model):
+    label =models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.label
+    
+class TaggedItemManager(models.Manager):
+    pass
+    
+class TaggedItem(models.Model):
+    tag = models.CharField(max_length=255)
+
+    content_type = models.ForeignKey(
+        ContentType,
+        on_delete=models.CASCADE
+    )
+
+    object_id = models.PositiveIntegerField()
+
+    content_object = GenericForeignKey(
+        'content_type',
+        'object_id'
+    )
